83b5720be5add7e845be511fdbbe819c40fce5d0
snowsee <ssnowsee@gmail.com>
2019-09-28 11:38:12 +0100

    More testing

--- rushwars/rushwars.py
+++ rushwars/rushwars.py
@@ -1754,8 +1754,9 @@ class RushWars(BaseCog):
 
         opponents = await self.config.all_users()
         for opponent in opponents:
-            return await ctx.send(opponent)
+            # return await ctx.send(opponent)
             opponent_stars = await self.get_stars(opponent)
+            return await ctx.send(f"Opp {opponent_stars}")
             if user_stars in range(opponent_stars-100, opponent_stars+1000): # set to 1000 for testing only
                 if opponents[opponent]["active"]["defenses"]:
                     selected = opponent
